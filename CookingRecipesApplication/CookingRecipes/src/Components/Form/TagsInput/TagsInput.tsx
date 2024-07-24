cd57451eabaec4b65a4304d985214f0931c33302
justproggramer <morozow2121@mail.ru>
2024-07-24 15:15:56 +0300

    commit
    manage with prettier

--- CookingRecipesApplication/CookingRecipes/src/Components/Form/TagsInput/TagsInput.tsx
+++ CookingRecipesApplication/CookingRecipes/src/Components/Form/TagsInput/TagsInput.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import "./TagsInput.css"
-import { FieldArray, FieldArrayRenderProps } from 'formik';
+import React, { useState } from "react";
+import "./TagsInput.css";
+import { FieldArray, FieldArrayRenderProps } from "formik";
 
 // Define the props interface
 interface TagsInputProps {
@@ -11,18 +11,18 @@ const TagsInput: React.FC<TagsInputProps> = ({ tags }) => {
   const [currentTags, setCurrentTags] = useState<string[]>(tags);
 
   const handleInputKeyPress = (
-    arrayHelpers: FieldArrayRenderProps, 
+    arrayHelpers: FieldArrayRenderProps,
     event: React.KeyboardEvent<HTMLInputElement>
   ): void => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       event.preventDefault(); // Prevent form submission
       const inputValue = (event.target as HTMLInputElement).value.trim(); // Get input value
 
       if (inputValue) {
         arrayHelpers.push(inputValue); // Add the tag to the array
         setCurrentTags([...currentTags, inputValue]); // Update local state
-        (event.target as HTMLInputElement).value = ''; // Clear the input
-        console.log('Enter pressed in input');
+        (event.target as HTMLInputElement).value = ""; // Clear the input
+        console.log("Enter pressed in input");
       }
     }
   };
@@ -33,25 +33,30 @@ const TagsInput: React.FC<TagsInputProps> = ({ tags }) => {
       render={(arrayHelpers) => (
         <div className="tags-input">
           <ul id="tags">
-            {currentTags && currentTags.length > 0 ? (
-              currentTags.map((tag, index) => (
-                <li key={index} className="tag">
-                  <span className='tag-title'>{tag}</span>
-                  <span className='tag-close-icon' onClick={() => {
-                    arrayHelpers.remove(index); // Remove the tag
-                    setCurrentTags(currentTags.filter((_, i) => i !== index)); // Update local state
-                  }}>
-                    x
-                  </span>
-                </li>
-              ))
-            ) : null}
+            {currentTags && currentTags.length > 0
+              ? currentTags.map((tag, index) => (
+                  <li key={index} className="tag">
+                    <span className="tag-title">{tag}</span>
+                    <span
+                      className="tag-close-icon"
+                      onClick={() => {
+                        arrayHelpers.remove(index); // Remove the tag
+                        setCurrentTags(
+                          currentTags.filter((_, i) => i !== index)
+                        ); // Update local state
+                      }}
+                    >
+                      x
+                    </span>
+                  </li>
+                ))
+              : null}
           </ul>
           <input
-            className='input'
+            className="input"
             type="text"
             placeholder="Press enter to add tags"
-            onKeyPress={e => handleInputKeyPress(arrayHelpers, e)}
+            onKeyPress={(e) => handleInputKeyPress(arrayHelpers, e)}
           />
         </div>
       )}
